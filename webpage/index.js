29710d4dc946b26d56a515565f614586f641b482
Colin Dugeai <colin.dugeai@gmail.com>
2023-02-17 14:27:05 +0100

    cleaning js code

--- webpage/index.js
+++ webpage/index.js
@@ -1,5 +1,7 @@
-// Returns schema of the indicator to validate against
-function getSchemaUrl(indicId_) {
+
+
+function getConfig() {
+
   // Hard coded array of schema urls
   const allSchemaUrl = {
     "ind-001":
@@ -10,66 +12,77 @@ function getSchemaUrl(indicId_) {
       "https://schema.data.gouv.fr/schemas/NaturalSolutions/schema-arbre/0.3.0/schema.json"
   };
 
-  return allSchemaUrl[indicId_];
+
+  return {
+    allSchemaUrl,
+    validateEndpoint: "https://api.validata.etalab.studio/validate",
+    idFileElement: "myFile"
+  }
+
 }
+
+// Return loaded File
+function getLoadedFile() {
+  return document.getElementById(getConfig().idFileElement).files[0];
+}
+
+
+// Returns schema of the indicator to validate against
+function getSchemaUrl(indicId_) {
+  return getConfig().allSchemaUrl[indicId_];
+}
+
+
+
 
 // Send file to validata API
 async function validateFile(file_, schemaUrl_) {
-  const validateEndpoint = "https://api.validata.etalab.studio/validate";
 
-  // Check https://api.validata.etalab.studio/apidocs
-  // TODO send file to validata API
-  let file = document.getElementById("myFile").files[0];
+  let file = getLoadedFile();
 
+
+  // Build form payload for validata API
   let data = new FormData();
   data.append("schema", schemaUrl_);
   data.append("file", file);
-  return fetch(validateEndpoint, {
+
+  // POST data = schemaUrl + File
+  return fetch(getConfig().validateEndpoint, {
     // Your POST endpoint
     method: "POST",
-    headers: {
-      // Content-Type may need to be completely **omitted**
-      // or you may need something
-      //"Content-Type": "multipart/form-data"
-    },
-    body: data // This is your file object
+    body: data
   })
-    .then(
-      (response) => response.json() // if the response is a JSON object
-    )
-    .then(
-      (success) => {
+    .then(r => r.json())
+    .then(rjson => {
         console.log("success");
-        return success;
-      } // Handle the success response object
+        return rjson;
+      }
     )
-    .catch(
-      (error) => {
+    .catch(error => {
         console.log(error);
-      } // Handle the error response object
+      }
     );
 }
 
+
+// When "validate data" button is clicked
 async function onValidateData(formData) {
+
   let res = {
-    indicId: formData.indicId.value,
-    userComment: formData.userComment.value,
-    filename: formData.filename.value,
-    fileObject: document.getElementById("myFile").files[0],
-    schemaUrl: getSchemaUrl(formData.indicId.value)
+    indicId: formData.indicId.value, // Id de l'indicateur
+    userComment: formData.userComment.value, // Commentaire utilisateur
+    filename: formData.filename.value, // Chemin du fichier
+    fileObject: getLoadedFile(),
+    schemaUrl: getSchemaUrl(formData.indicId.value) // Url du schema
   };
-
-  const selectedFile = document.getElementById("myFile").value;
-
-  console.log(selectedFile);
 
   console.log({ res });
 
   let isFileValidResponse = await validateFile(res.fileObject, res.schemaUrl);
-  console.log(isFileValidResponse);
   let isFileValid = isFileValidResponse.report.valid;
 
   console.log(isFileValid);
+  // Display results
   alert(
     JSON.stringify(
       Object.assign(res, {
@@ -80,12 +93,18 @@ async function onValidateData(formData) {
       4
     )
   );
+
+
   if (isFileValid) {
-    // TODO Enable button "Import Data"
     document.getElementById("resultLabel").textContent = "OUI";
-  } else document.getElementById("resultLabel").textContent = "NON";
+    // TODO Enable button "Import Data"
+  } else {
+    document.getElementById("resultLabel").textContent = "NON";
+    // TODO Disable button "Import Data"
+  }
 }
 
 function onImportData(formData) {
   alert("ImportData triggered");
+  // TODO Push data to db
 }
